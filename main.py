2d538cd8458e5819fb7087708319b1271cc3da5a
Renato Gonçalves <renatogoncalves@Renatos-MacBook-Pro.local>
2021-01-29 11:24:10 -0300

    Implement snake movement control.

--- main.py
+++ main.py
@@ -11,12 +11,19 @@ snake = Snake()
 screen.update()
 time.sleep(1)
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 game_on = True
 
 while game_on:
     screen.update()
-    time.sleep(0.3)
+    time.sleep(0.1)
 
     snake.move()
+
 
 screen.exitonclick()
